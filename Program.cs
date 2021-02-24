e80621db4046320c22f737d548e1ac4d36e9b498
kelvin.mozart <kelvin.mozart@live.com>
2021-02-24 10:30:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TestaPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaPrimo valor = new TestaPrimo();
+            Stopwatch sw = new Stopwatch();
+
+            var n = 2147483647;
+            int primo = valor.Primo(n);
+            int[] vetor;
+            vetor = new int[10];
+            //var count = 0;
+
+            if (primo == 1)
+                Console.WriteLine("Eh Primo");
+            else
+                Console.WriteLine("Nao eh Primo");
+
+            for(int i=0; i<10; i++)
+            {
+                sw.Start();
+                valor.Primo(n);
+                sw.Stop();
+                vetor[i] = (int)sw.ElapsedTicks;
+                Console.WriteLine($"Tempo {i}: {sw.ElapsedTicks}ms");
+            }
+            var media = 0;
+            var soma = 0;
+            for(int j=0; j<10; j++)
+            {
+                soma = vetor[j] + soma;
+            }
+            media = soma / 10;
+            
+            Console.WriteLine($"Tempo passado: {media}ms");
+        }
+    }
+}
---
+++ TestaPrimo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestaPrimo
+{
+    class TestaPrimo
+    {
+        public int Primo(int n)
+        {
+            var ehPrimo = 1;
+            var d = 2;
+    
+            if (n <= 1)
+    	        ehPrimo = 0;
+
+            while (ehPrimo == 1 && d <= n /2) {
+    	        if (n % d  == 0)
+        	        ehPrimo = 0;
+    	        d = d + 1;
+            }
+            return ehPrimo;
+        }
+        
+    }
+}
